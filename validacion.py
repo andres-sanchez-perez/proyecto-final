a122ee6050c238b9e5ac7fbf5f2f32185f646c6b
andres-sanchez-perez <55764738+andres-sanchez-perez@users.noreply.github.com>
2019-11-26 18:45:04 -0500

    Update validacion.py

--- validacion.py
+++ validacion.py
@@ -2,8 +2,8 @@ while True:
     print("menu\n1 para ingresar nueva ficha\n2 para buscar una ficha medica\n")
     opcion3=str(input("ingrese una opcion:"))
     if(opcion3.isdigit()==True):
+        opcion3_int=int(opcion3)
         if(opcion3>0):
-            opcion3_int=int(opcion3)
             break
         else:
             print("ingrese una opcion valida")
