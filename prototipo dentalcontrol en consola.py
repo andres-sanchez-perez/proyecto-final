972cba5203e69bbf84a52c1f9a10de229e034869
andres-sanchez-perez <55764738+andres-sanchez-perez@users.noreply.github.com>
2019-11-27 16:41:41 -0500

    Update prototipo dentalcontrol en consola.py

--- prototipo dentalcontrol en consola.py
+++ prototipo dentalcontrol en consola.py
@@ -4,21 +4,10 @@ import os
 
 def paciente(opcion):
     ingresomatrizpacientes()
-def inventario(opcion):
-    while True:
-        print("menu\n1 para agregar un nuevo producto\n2 para buscar un producto\n3 para modificar un elemento\n4 para eliminar un producto\n5 para volver al menu principal")
-        opcion3=int(input("ingrese su opcion:"))
-        if (opcion3==1):
-            ingresoelemento()
-        elif (opcion3==2):
-            buscarelemento()
-        elif (opcion3==3):
-            modificarelelemento()
-        elif (opcion3==4):
-            eliminarelemento()
-        if(opcion3>=5):
-            break
 
+def inventario(opcion):
+    ingresoelemento()
+        
 def facturacion(opcion):
     while True:
         print("menu\n1 para crear nueva factura\n2 para volver al menu principal")
@@ -46,8 +35,7 @@ def ingresomatrizpacientes():
             else:
                 print("ingrese una opcion valida")
                 pause()
-                os.system("cls")
-                ##el "cls" es para usar en windows, "clear" para UNIX, es decir linux y onlinegdb##
+                os.system("clear")
             
             
         if(opcion3_int==1):
@@ -60,15 +48,13 @@ def ingresomatrizpacientes():
                 for j in range(m):
                     if(s==1):
                         q="un nombre"
-                        p=str(input("ingrese el nombre del paciente:"))
-                        p=validacionalfabetica(p,q)
+                        p=validacionalfabetica(q)
                     if(s==2):
                         q="una edad"
-                        p=int(input("ingrese la edad del paciente:"))
+                        p=validacionnumerica(q)
                     if(s==3):
                         q="un genero"
-                        p=str(input("ingrese el genero del paciente:"))
-                        p=validacionalfabetica(p,q)
+                        p=validacionalfabetica(q)
                     if(s==4):
                         while True:
                             p=str(input("ingrese la cedula del paciente:"))
@@ -77,49 +63,62 @@ def ingresomatrizpacientes():
                                     break
                             else:
                                 print("ingrese una cedula valida")
-                                pause()
+                                wait_for("presione una tecla para continuar")
                                 os.system("clear")
                     if(s==5):
                         q="un diagnostico"
-                        p=str(input("ingrese el diagnostico del paciente:"))
-                        p=validacionalfabetica(p,q)
+                        p=validacionalfabetica(q)
                     if(s==6):
-                        q="un tratamiento"
-                        p=str(input("ingrese el tratamiento a seguir:"))
-                        p=validacionalfabetica(p,q)
+                        p=input("ingrese el tratamiento aqui:")
                     a[i].append(p)
                     s=s+1
                 print(a)
-                wait_for("Presione una tecla para continuar")
+                pause()
                 print("si quiere ingresar otra ficha presiones cualquier boton\ncaso contrario presione 1")
                 desicion=int(input("ingrese su desicion aquí:"))
                 if(desicion==1):
                     break
+
+    
         
 def ingresoelemento():
-    n=10
-    m=4
-    s=1
-    a=[]
-    for i in range(n):
-        a.append([])
-        for j in range(m):
-            if (s==1):
-                p=int(input("ingrese el numero identificador del producto que ingresara:"))
-            if (s==2):
-                p=str(input("ingrese el nombre del producto que ingresara:"))
-            if (s==3):
-                p=str(input("ingrese el tipo de producto que ingresara:"))
-            if (s==4):
-                p=int(input("ingrese la cantidad del producto que tiene:"))
-            a[i].append(p)
-            s=s+1
-        print(a)
-        wait_for("Presione una tecla para continuar")
-        print("si quiere ingresar otro producto presione cualquier tecla\ncaso contrario presione 1")
-        desicion=int(input("ingrese su desicion aqui:"))
-        if(desicion==1):
-            break
+    while True:
+        while True:
+            print("menu\ningrese 1 para ingresar un nuevo prodcuto\ningrese 2 para buscar un prodcuto\ningrese 3 para volver al menu principal")
+            opcion4=str(input("ingrese una opcion:"))
+            if(opcion4.isdigit()==True):
+                opcion4_int=int(opcion4)
+                if(opcion4_int>0):
+                    break
+            else:
+                print("ingrese una opcion valida")
+                pause()
+                os.system("clear")
+        if(opcion4_int==1):
+            n=10
+            m=4
+            s=1
+            a=[]
+            for i in range(n):
+                a.append([])
+                for j in range(m):
+                    if (s==1):
+                        p=int(input("ingrese el numero identificador del producto que ingresara:"))
+                    if (s==2):
+                        p=str(input("ingrese el nombre del producto que ingresara:"))
+                    if (s==3):
+                        p=str(input("ingrese el tipo de producto que ingresara:"))
+                    if (s==4):
+                        p=int(input("ingrese la cantidad del producto que tiene:"))
+                    a[i].append(p)
+                    s=s+1
+                print(a)
+                pause()
+                s=1
+                print("si quiere ingresar otro producto presione cualquier tecla\ncaso contrario presione 1")
+                desicion=int(input("ingrese su desicion aqui:"))
+                if(desicion==1):
+                    break
 def matrizfactura():
     acu=0
     n=10
@@ -137,7 +136,7 @@ def matrizfactura():
             a[i].append(p)
             s=s+1 
         print(a)
-        wait_for("Presione una tecla para continuar")
+        pause()
         s=1    
         print("¿Desea ingresar otro procediemiento?\nen caso de serlo presione cualquier boton\ncaso contrario presione 1")
         desicion=int(input("ingrese su desicion:"))
@@ -152,20 +151,35 @@ def matrizfactura():
     print("el costo total de los procedimientos es:",total)
     pause()
     
-def validacionalfabetica(dato,nombre):
+def validacionalfabetica(nombre):
     while True:
+        print("ingrese",nombre,"del paciente\n")
+        dato=input("ingrese aqui el dato pedido:")
         if(dato.isalpha()==True):
             break
         else:
             print("ingrese",nombre,"valido")
             pause()
-            os.system("cls")
-        dato=str(input("vuelva a ingresar aqui:"))
+            os.system("clear")
     return dato
-##lo siguiente es el presione enter para continuar sin tener que usar la libreria termios, ya que esta libreria solo existe en UNIX,linux y onlinegdb##
+
+def validacionnumerica(nombre):
+    while True:
+        print("ingrese",nombre,"del paciente\n")
+        dato=input("ingrese el dato pedido aqui:")
+        if(dato.isdigit()==True):
+            dato_int=int(dato)
+            if(dato_int>0):
+                break
+        else:
+            print("ingrese",nombre,"valido")
+            pause()
+            os.system("clear")
+    return dato_int
+
 def pause():
-    pause=input("presione enter para continuar")
-    
+    pause=input("presione enter para continuar")##lo siguiente es el presione enter para continuar sin tener que usar la libreria termios, ya que esta libreria solo existe en UNIX,linux y onlinegdb##
+
 while True:
     print("menu principal\n1 para pacientes\n2 para inventario\n3 para facturacion\n4 para calendario\n5 para salir\n")
     opcion=int(input("ingrese su opcion:"))
@@ -183,3 +197,5 @@ while True:
         
     if(opcion>=5):
         break
+
+
